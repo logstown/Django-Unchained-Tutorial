e6feb10e63aa57512e242d9ebd46ea5096f10abf
LJLinux <ljoecks@gmail.com>
2013-04-10 16:26:43 -0700

    finished templating lesson

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -1,5 +1,8 @@
 # Django settings for hackernews project.
 
+import os
+PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -61,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -69,9 +72,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'static'),   
 )
 
 # List of finder classes that know how to find static files in
@@ -108,9 +109,7 @@ ROOT_URLCONF = 'hackernews.urls'
 WSGI_APPLICATION = 'hackernews.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
